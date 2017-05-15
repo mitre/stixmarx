81b77693b7017721786ed85f219891e61a100ed7
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2017-05-15 13:06:24 -0400

    General improvements. Added handler for logging library messages.

--- stixmarx/__init__.py
+++ stixmarx/__init__.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2017, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
+# builtin
+import logging
+
 # internal
 from stixmarx.version import __version__
+
+# Console Handler for stixmarx messages
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter("[%(name)s] [%(levelname)-7s] [%(asctime)s] %(message)s"))
+
+# Module-level logger
+log = logging.getLogger(__name__)
+log.addHandler(ch)
 
 
 def parse(xml_input, encoding=None):
